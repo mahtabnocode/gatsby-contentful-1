cea62552344d75a9eb9ae4d8470dada902e65cdc
mahtabnocode <133096181+mahtabnocode@users.noreply.github.com>
2023-05-11 13:43:31 +0330

    wdawd

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -1,14 +1,7 @@
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-categories-contentful-category-id-js": () => import("./../../../src/pages/categories/{ContentfulCategory.id}.js?export=default" /* webpackChunkName: "component---src-pages-categories-contentful-category-id-js" */),
-  "component---src-pages-image-api-js": () => import("./../../../src/pages/image-api.js?export=default" /* webpackChunkName: "component---src-pages-image-api-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-products-contentful-product-id-js": () => import("./../../../src/pages/products/{ContentfulProduct.id}.js?export=default" /* webpackChunkName: "component---src-pages-products-contentful-product-id-js" */)
-}
-
-
-
-exports.head = {
-
+  "component---src-pages-categories-contentful-category-id-js": () => import("./../../../src/pages/categories/{ContentfulCategory.id}.js" /* webpackChunkName: "component---src-pages-categories-contentful-category-id-js" */),
+  "component---src-pages-image-api-js": () => import("./../../../src/pages/image-api.js" /* webpackChunkName: "component---src-pages-image-api-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-products-contentful-product-id-js": () => import("./../../../src/pages/products/{ContentfulProduct.id}.js" /* webpackChunkName: "component---src-pages-products-contentful-product-id-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\AdminTL\\OneDrive\\Documents\\GitHub\\gatsby-contentful-1\\.cache\\dev-404-page.js")),
   "component---src-pages-categories-contentful-category-id-js": preferDefault(require("C:\\Users\\AdminTL\\OneDrive\\Documents\\GitHub\\gatsby-contentful-1\\src\\pages\\categories\\{ContentfulCategory.id}.js")),
   "component---src-pages-image-api-js": preferDefault(require("C:\\Users\\AdminTL\\OneDrive\\Documents\\GitHub\\gatsby-contentful-1\\src\\pages\\image-api.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Users\\AdminTL\\OneDrive\\Documents\\GitHub\\gatsby-contentful-1\\src\\pages\\index.js")),
